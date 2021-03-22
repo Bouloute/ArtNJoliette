83c18a2d9ee179cdff974db119fc711d12a15381
Bouloute <annaick1995@gmail.com>
2021-03-22 18:43:19 -0400

    adding artists to data

--- src/data/arts.js
+++ src/data/arts.js
@@ -7,10 +7,10 @@ export const arts = [
         "enigma": "Ceci est un test. Les bonnes reponses sont: ours et ourse",
         "answers": "ours, ourse, Ours, Ourse",
         "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.825128806022!2d5.36035711549159!3d43.29698367913515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c13e9876806d%3A0x575fa16048e0502d!2sEsplanade%20du%20J4!5e0!3m2!1sen!2sus!4v1611776148724!5m2!1sen!2sus",
-        "artist_id": 1,
         "created_at": "2021-01-28T17:35:59.482Z",
         "updated_at": "2021-01-28T17:35:59.550Z",
         "artist": {
+            "id": 4,
             "name": "Jayet"
         }
     },
@@ -22,26 +22,21 @@ export const arts = [
         "enigma": "Ceci est un test. Les bonnes reponses sont: bob et bobby",
         "answers": "bob, bobby",
         "map_url": "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2903.2492035609093!2d5.365396015491966!3d43.30904242913455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPassage%20Henri%20Verneuil!5e0!3m2!1sen!2sus!4v1611854942498!5m2!1sen!2sus",
-        "artist_id": 2,
-        "created_at": "2021-01-28T17:35:59.567Z",
-        "updated_at": "2021-01-28T17:35:59.603Z",
         "artist": {
+            "id": 3,
             "name": "Astro"
         }
     },
     {
         "id": 3,
-        "name": "Seconde Nature",
-        "description": "Drapeau orange",
+        "name": "Seconde Vie",
+        "description": "A l’origine, cette œuvre commandée par Euroméditerranée en 2010, était celle de Miguel Chevalier, artiste digital, et Charles Bové, designer. Nommée Seconde Nature, cette sculpture inclinée était constituée d’une statue d’acier de 18 mètres de haut et de 3 vidéoprojecteurs. Une œuvre hybride, mêlant réel et virtuel, qui projetait sur la façade des docks la flore méditerranéenne évoluant au fil des saisons. Les promeneurs pouvaient découvrir tantôt des palmiers, tantôt des plantes grasses, qui s’inclinaient à leur passage. Flamboyante et enroulée sur elle-même, la statue évoque la forme d’un coquillage, le vent qui s'engouffre dans la voile des bateaux. Au fur et à mesure, la sculpture s’est dégradée, œuvre du temps ou des tags des passants. Le duo d’artistes a alors demandé à l’association Planètes Émergences, qui aide à la réhabilitation d’espaces communs en leur apportant une touche artistique, de lancer un appel à projet pour restaurer l'œuvre, lui donner un nouveau souffle, une Seconde Vie. Ipin, street artiste, s’intègre au projet et décide de s’inspirer de la \"lutte anti graffiti\" qui court les rues : une pratique des politiques urbaines qui consiste à recouvrir les graffitis par l'application d'une couche de peinture de teinte plus ou moins proche de la couleur originelle du mur. Jouant avec les tags présents et les “patchs” de peinture imposés par la municipalité pour les dissimuler, il crée une mosaïque de carrés orange qui s’élève et s’organise. Les œuvres d’art sont le reflet d’un territoire. A l’image du quartier, Seconde Nature devenue Seconde Vie n’a cessé de se métamorphoser. Qui sait ce qu’elle deviendra ? ",
         "image_url": "https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Seconde%20Vie%2F143000436_232308558490095_5211856471468779632_n.jpg?alt=media&token=d79eaab9-7c0e-40cd-947a-c90119d7a68b",
         "enigma": "Ceci est un test. Les bonnes reponses sont: a, b et c",
         "answers": "a, b, c",
-        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1451.6902399761411!2d5.365507658201738!3d43.306293994783665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0ee9560ef01%3A0xebe89f1d6ddb7d14!2sQ-Park%20Les%20Docks%20-%20arvieux!5e0!3m2!1sen!2sus!4v1616206799296!5m2!1sen!2sus",
-        "artist_id": 3,
-        "created_at": "2021-01-28T17:35:59.619Z",
-        "updated_at": "2021-01-28T17:35:59.651Z",
-        "artist": {
-            "name": "Germain alias Ilpin"
+        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1451.6902399761411!2d5.365507658201738!3d43.306293994783665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0ee9560ef01%3A0xebe89f1d6ddb7d14!2sQ-Park%20Les%20Docks%20-%20arvieux!5e0!3m2!1sen!2sus!4v1616206799296!5m2!1sen!2sus",        "artist": {
+            "id": 2,
+            "name": "Ipin alias Germain"
         }
     },
     {
@@ -52,11 +47,48 @@ export const arts = [
         "enigma": "Ceci est un test. Les bonnes reponses sont: azerty et azert",
         "answers": "azerty, azert",
         "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.7339445360412!2d5.361296915491653!3d43.298893079135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0c2c6b19e5f%3A0xe693936cd108d2b6!2sBoulevard%20Jacques%20Saade%2C%2013002%20Marseille%2C%20France!5e0!3m2!1sen!2sus!4v1611855036218!5m2!1sen!2sus",
-        "artist_id": 4,
-        "created_at": "2021-01-28T17:35:59.666Z",
-        "updated_at": "2021-01-28T17:35:59.700Z",
         "artist": {
-            "name": "Bruno Catalane"
+            "id": 1,
+            "name": "Bruno Catalano"
+        }
+    }
+]
+
+export const artists = [
+    {
+        "id": 1,
+        "name": "Bruno Catalano",
+        "description": "Bruno Catalano est sculpteur français né à Khouribga au Maroc en 1960.",
+        "image_url": "https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Ours%20Biface%2Fourse.jpg?alt=media&token=92778884-ff46-42a4-9d35-126afd0fbc74",
+        "art": {
+            "id": 4
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ipin",
+        "description": "Fresque",
+        "image_url": "https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Evasion%2F142270641_2861122120773263_7514090664490430334_n.jpg?alt=media&token=8ac125f4-d07a-46d6-be87-a99371460d54",
+        "art": {
+            "id": 3
+        }
+    },
+    {
+        "id": 3,
+        "name": "Astro",
+        "description": "Astro est un street artiste français né à Sarcelles en 1981.",
+        "image_url": "https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Evasion%2F142270641_2861122120773263_7514090664490430334_n.jpg?alt=media&token=8ac125f4-d07a-46d6-be87-a99371460d54",
+        "art": {
+            "id": 2
+        }
+    },
+    {
+        "id": 4,
+        "name": "Jayet",
+        "description": "",
+        "image_url": "https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Evasion%2F142270641_2861122120773263_7514090664490430334_n.jpg?alt=media&token=8ac125f4-d07a-46d6-be87-a99371460d54",
+        "art": {
+            "id": 1
         }
     }
 ]
