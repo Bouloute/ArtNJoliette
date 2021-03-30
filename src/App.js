7d8ea9bc166aac7b76817542027d7aebc011c7ae
Bouloute <annaick1995@gmail.com>
2021-03-29 21:58:24 -0400

    added logo + Playing around with css to make it work

--- src/App.js
+++ src/App.js
@@ -21,8 +21,12 @@ class App extends Component {
         
 
         <Router>
-          <NavLink to="/">
-            <h1 className="title">Art N' Joliette</h1>
+          <NavLink to="/" className="title">
+            
+            <img src="https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Logo.png?alt=media&token=07306777-6073-4856-add4-13032b091de2"></img>
+            <h1 >Art N' Joliette</h1>
+            <img src="https://firebasestorage.googleapis.com/v0/b/art-n-joliette.appspot.com/o/Logo.png?alt=media&token=07306777-6073-4856-add4-13032b091de2" className="right"></img>
+
           </NavLink>
           
           <Route exact path="/" component={Home} />
