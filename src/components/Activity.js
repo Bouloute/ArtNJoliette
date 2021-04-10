fb3b44cd07f4a37c408a084e81b6c47a33bb8d17
Bouloute <annaick1995@gmail.com>
2021-04-09 20:36:31 -0400

    Gallery prettiness

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -4,8 +4,11 @@ import Gallery from './Gallery'
 class Activity extends Component {
     render() {
         return (
-            <div>
-                <Gallery/>
+            <div className="quizz">
+                <div className="quizz-title ">
+                    <h2>Sortie du 24 mars en partenariat avec le centre Leo Lagrange</h2>
+                    <Gallery/>
+                </div>
             </div>
         );
     }
